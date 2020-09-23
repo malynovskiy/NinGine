39faac5eaa94f06665b852662d60d66b5954b2b7
Maksym Malynovskyi <maksym.malynovskiy@gmail.com>
2020-09-23 09:08:16 +0300

    Fixed code style a bit

--- ningine.cpp
+++ ningine.cpp
@@ -300,7 +300,13 @@ GLuint createShaderProgram(GLuint vertexShader, GLuint fragmentShader)
 
 void processInput(GLFWwindow *window)
 {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
 }
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) { glViewport(0, 0, width, height); }
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
