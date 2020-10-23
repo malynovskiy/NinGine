b1c723c4be97fc36b3a7edc8b6ea7f86bb627048
Maksym Malynovskyi <maksym.malynovskiy@gmail.com>
2020-10-23 22:30:49 +0300

    wip

--- src/math/Vector.h
+++ src/math/Vector.h
@@ -38,7 +38,51 @@ namespace math
       return os;
     }
 
-    Vector2<T> operator+(const Vector2<T> &v) {}
+    Vector2<T> operator+(const Vector2<T> &v) const
+    {
+      assert(!v.hasNaNs());
+      return Vector2<T>(x + v.x, y + v.y);
+    }
+
+    Vector2<T> operator-(const Vector2<T> &v) const
+    {
+      assert(!v.hasNaNs());
+      return Vector2<T>(x - v.x, y - v.y);
+    }
+
+    Vector2<T> &operator+=(const Vector2<T> &v)
+    {
+      assert(!v.hasNaNs());
+      x += v.x;
+      y += v.y;
+      return *this;
+    }
+
+    Vector2<T> &operator-=(const Vector2<T> &v)
+    {
+      assert(!v.hasNaNs());
+      x -= v.x;
+      y -= v.y;
+      return *this;
+    }
+
+    bool operator==(const Vector2<T> &v) const { return x == v.x && y == v.y; }
+    bool operator!=(const Vector2<T> &v) const { return x != v.x || y != v.y; }
+
+    Vector2<T> operator*(T t) const { return Vector2(x * t, y * t); }
+    Vector2<T> &operator*=(T t) 
+    { 
+      assert(!std::isnan(t));
+      x *= t;
+      y *= t;
+      return *this;
+    }
+
+    Vector2<T> operator/(T t) const 
+    { 
+      return Vector2(x * t, y * t); 
+    }
+    
 
     T x, y;
   };
